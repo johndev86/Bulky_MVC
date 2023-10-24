e98b6a9a046e52ab27f056da0f7b80f442e7529a
J Lee <44005960+johndev86@users.noreply.github.com>
2023-10-24 09:57:13 -0400

    Rename to BulkyBook

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
 ﻿
-using Bulky.Models;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BulkyWeb.Controllers
+namespace BulkyBookWeb.Controllers
 {
     public class HomeController : Controller
     {
